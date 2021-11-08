1cbe009ff8bed891db10f7b495bcfa55bb8fcbf9
Michel Dávalos Boites <michel.davalos@academicos.udg.mx>
2021-11-08 12:11:54 -0600

    agregado .gitignore

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    ListaDobleLigada<int> enteros;
+   /* ListaDobleLigada<int> enteros;
 
     enteros.push_front(1);
     enteros.push_front(2);
@@ -49,6 +49,34 @@ int main() {
     enteros.~ListaDobleLigada();
     cout << "cantidad: " << enteros.size() << endl;*/
 
+    ListaDobleLigada<int> enteros;
+
+    enteros.push_front(1);           // insertar al inicio (frente)
+    enteros.push_front(2);           // insertar al inicio (frente)
+    enteros.push_front(3);           // insertar al inicio (frente)
+
+    enteros << 4 << 5 << 6;          // insertar al final (cola)
+
+    cout << enteros.size() << endl;  // imprime la cantidad de nodos (elementos)
+
+    enteros.print();                 // imprime el elemento de cada nodo
+
+    int *ptr_01 = enteros.find(1); // buscar un elemento
+    if (ptr_01) {                    // si no es nulo
+        *ptr_01 = 3;                 // cambiar valor
+    }
+
+    int *ptr_02 = enteros.find(0);   // buscar un elemento
+    if (ptr_02 == nullptr) {         // si es nulo
+        enteros << 0;                // inserta al final
+    }
+
+    cout << enteros.size() << endl;  // imprime la cantidad de nodos (elementos)
+
+    enteros.print();                 // imprime el elemento de cada nodo
+    
+    return 0;
+}
 
     return 0;
 }
